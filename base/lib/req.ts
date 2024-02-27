979a37a573207b60da7ddf900e535ecfd8467160
Nikechukwu <nikechukwu@gmail.com>
2024-02-26 20:56:40 -0500

    Documentation

--- base/lib/req.ts
+++ base/lib/req.ts
@@ -1,9 +1,37 @@
+/**
+ * A class used to represent the data gotten from a route path, such as the route path, queries, parameters and hashes.
+ * 
+ * This is the class type for the `req` parameter included in every route callback function.
+ */
 export class RouteResult {
+    /**
+     * The name of the uri path (real name - not with parameter regex)
+     * @type {string}
+     */
     name: string;
+    /**
+     * An object to represent the query parameters of a uri path
+     * @type {object | undefined}
+     */
     query: object | undefined;
+    /**
+     * Any parameters parsed from the uri path via the given parameter regex
+     * @type {object | undefined}
+     */
     params: object | undefined;
+    /**
+     * Any hash parsed from the uri path
+     * @type {string | undefined}
+     */
     hash: string | undefined;
 
+    /**
+     * Constructor to instantiate a RouteResult Object.
+     * @param {string} name - The name of the path
+     * @param {object} query - The query parameters from the path
+     * @param {object} params - The path parameters gotten
+     * @param {string} hash - The hash from the path
+     */
     constructor(name: string, query?: object, params?: object, hash?: string) {
         this.name = name;
         this.query = query;
@@ -12,6 +40,10 @@ export class RouteResult {
     }
 }
 
+/**
+ * TODO: Implement this class.
+ * This class is not for use yet.
+ */
 export class RouteRequest {
     status: number;
     headers: object | undefined;
--- base/lib/router.ts
+++ base/lib/router.ts
@@ -4,13 +4,57 @@ import { createRegExpFromPath } from './utils/Extract.ts';
 import { getRouteRequest } from './utils/Request.ts';
 
 // deno-lint-ignore-file ban-types
+/**
+ * Class implementation of a Router.
+ * 
+ * This is a class used to instantiate a web router. 
+ * This router has the power to create multiple kinds of routes. These routes can be static like `"/state"` or having parameters like `"/:id"` to get the `id`.
+ * 
+ * ```typescript
+ * let router = new Router();
+ * router.get('/', (req) => {
+ *   console.log("Hello World");
+ * });
+ * router.get('/:id', (req) => {
+ *   console.log(`ID: ${req.params.id}`);
+ * });
+ * router.init();
+ * ```
+ * 
+ * More information on functionality is in the function definitions.
+ */
 class Router {
+    /**
+     * The list of routes in a router.
+     * @type {Route[]}
+     */
     routes: Route[];
 
+    /**
+     * Constructor to instantiate a new router.
+     * As of now, the constructor creates an empty list of routes.
+     */
     constructor() {
         this.routes = [];
     }
 
+    /**
+     * Function to create and register a new route.
+     * 
+     * This function is used in registering a new `Route` object that runs the `callback` function whenever the web page navigates to the route described in the `uri` parameter.
+     * 
+     * ```typescript
+     * route.get('/seats/:no', (req) => {
+     *   let queryObj = req.query
+     * });
+     * ```
+     * 
+     * The function is of type `RouteFunc` which takes in a parameter called `req`. 
+     * This parameter contains information about the path, such as defined parameters (`req.params`), queries (`req.query`) and hashes (`req.hash`).
+     * 
+     * @param {string} uri - The uri path of the router function.
+     * @param {RouteFunc} callback - The function called when the window moves to the given uri path.
+     */
     get(uri: string, callback: RouteFunc) {
         if(!uri || !callback) throw new Error('uri or callback must be given');
 
@@ -27,6 +71,11 @@ class Router {
         this.routes.push(route);
     }
 
+    /**
+     * Initialises the router.
+     * 
+     * This function registers the router and renders the code provided in each route's callback function when the window moves to the given path.
+     */
     init(){
         this.routes.some(route=>{
 
