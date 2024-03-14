8cb4d006c6a92e324b62d35fa9eabe49ccbf73d8
Nikechukwu <nikechukwu@gmail.com>
2024-03-14 18:31:07 -0400

    New entrypoint

--- base/mod.ts
+++ base/mod.ts
@@ -1,6 +1,26 @@
-import Router from './lib/router.ts'
+import {
+    Router, 
+    SPARouter
+} from './lib/router.ts';
+import {
+    navigateTo,
+    navigateBack,
+    pushEvent
+} from './lib/src/spa/navigation.js'
 
 // allow browser/dart to access object
-Object.assign(window, { Router: Router });
+Object.assign(window, { 
+    Router: Router,
+    SPARouter: SPARouter,
+    navigateTo: navigateTo,
+    navigateBack: navigateBack,
+    pushEvent: pushEvent
+});
 
-export { Router };
+export {
+    Router,
+    SPARouter,
+    navigateBack,
+    navigateTo,
+    pushEvent
+};
