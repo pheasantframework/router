b9f7ad1a508dfefc7cc10b436993f261b060ee4c
Nike <nikechukwu@gmail.com>
2024-03-14 23:28:32 -0400

    Created Dart Interface

---
+++ lib/src/router/router.dart
@@ -0,0 +1,36 @@
+import 'dart:js_interop';
+import 'js/spa_router.dart';
+
+import 'js/router.dart';
+
+class Router {
+  final JSRouter _router;
+
+  Router([String initialLocation = '/']): _router = JSRouter(initialLocation);
+
+  void get(String uri, RouterFunction function) {
+    _router.get(uri, (function.jsify() as JSFunction));
+  }
+
+  void init() {
+    _router.init();
+  }
+}
+
+class SPARouter {
+  final JSSPARouter _router;
+
+  SPARouter([String initialLocation = '/']): _router = JSSPARouter(initialLocation);
+
+  void get(String uri, SPARouterFunction function) {
+    _router.get(uri, (function.jsify() as JSFunction));
+  }
+
+  void init() {
+    _router.init();
+  }
+}
+
+typedef RouterFunction = void Function(dynamic req);
+
+typedef SPARouterFunction = void Function(dynamic req, dynamic state);
