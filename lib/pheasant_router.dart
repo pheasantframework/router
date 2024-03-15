f67439cb05fcad74aacb193cc1f5f2d6557c3efa
Nike <nikechukwu@gmail.com>
2024-03-14 23:29:01 -0400

    Updated Files

--- lib/pheasant_router.dart
+++ lib/pheasant_router.dart
@@ -1,8 +1,8 @@
 /// Support for doing something awesome.
 ///
 /// More dartdocs go here.
-library;
+library pheasant_router;
 
-export 'src/pheasant_router_base.dart';
+export 'src/base.dart';
 
 // TODO: Export any libraries intended for clients of this package.
