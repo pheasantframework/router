ace339a4d17e9d03ce32d6c9ed9a92624953c6a1
Nike <nikechukwu@gmail.com>
2024-03-14 23:28:51 -0400

    Awaiting New Pheasant Version

--- lib/components.dart
+++ lib/components.dart
@@ -1,18 +1,20 @@
 import 'package:pheasant/custom.dart';
 
-/// This is the name of your plugin component.
-/// 
-/// Change this, or make a function to call the constructor in order to change the name of the component when rendered
-/// 
-/// For more guidelines on making your custom component check: https://github.com/pheasantframework/pheasant/blob/patch/docs/custom/components.md
-class PlaceholderPlugin extends PheasantComponent {
-  // You can add attribute definitions as well to your component
 
-  // This function must always be overriden to provide the element.
+class RouterView extends PheasantComponent {
   @override
   Element renderComponent([TemplateState? state]) {
-    return ParagraphElement()..text = "Hello World!";
+    // TODO: implement renderComponent
+    throw UnimplementedError();
   }
+  
 }
 
-// You can add more components, but it is recommended to export them all into one file for ease of access by the framework renderer.
+class RouterLink extends PheasantComponent {
+  @override
+  Element renderComponent([TemplateState? state]) {
+    // TODO: implement renderComponent
+    throw UnimplementedError();
+  }
+  
+}
