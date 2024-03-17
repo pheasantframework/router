c81ab428d6b4b95ced2bf47a5b374de3d3bed765
Nikechukwu <nikechukwu@gmail.com>
2024-03-16 21:09:09 -0400

    Modified Type Decs

--- base/dist/pheasant_routing.d.ts
+++ base/dist/pheasant_routing.d.ts
@@ -1,17 +1,49 @@
+// deno-lint-ignore-file no-explicit-any
+type SPAState = {
+    details: object | undefined;
+    [key: string]: any;
+};
+
+declare class RouteResult {
+    name: string;
+    query: object | undefined;
+    params: object | undefined;
+    hash: string | undefined;
+}
+
+type RouteFunc = (request: RouteResult) => void;
+type SPARouteFunc = (request: RouteResult, state: SPAState) => void;
+
+interface BaseRoute {
+    uri: string;
+    // deno-lint-ignore ban-types
+    callback: Function;
+}
+
+interface Route extends BaseRoute {
+    uri: string;
+    callback: RouteFunc;
+}
+
+interface SPARoute extends BaseRoute {
+    uri: string;
+    callback: SPARouteFunc
+}
+
 export class Router {
     constructor(initialRoute?: string);
-    routes: any[];
+    routes: Route[];
     initialRoute: string;
-    get(uri: any, callback: any): void;
+    get(uri: string, callback: RouteFunc): void;
     init(): void;
 }
 export class SPARouter {
     constructor(initialRoute?: string);
-    routes: any[];
+    routes: SPARoute[];
     initialRoute: string;
-    get(uri: any, callback: any): void;
+    get(uri: string, callback: SPARouteFunc): void;
     init(): void;
 }
 export function navigateBack(): void;
-export function navigateTo(uri: any, state: {}, details: any): void;
+export function navigateTo(uri: string, state: object, details: any): void;
 export const pushEvent: CustomEvent<any>;
