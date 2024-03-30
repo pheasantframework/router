fc5cd97d0217c8ace2d12525e225b6b836f4beb9
Nike <nikechukwu@gmail.com>
2024-03-29 22:46:06 -0400

    Added App Extension

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,1 +1,17 @@
 import 'package:pheasant/custom.dart';
+import 'package:pheasant_router/src/prereq/tags.dart';
+import 'package:pheasant_router/src/router/router.dart';
+
+class PheasantRouterApp extends PheasantBaseApp {
+  Router _router;
+
+  PheasantRouterApp(super.app, {Router? router})
+      : _router = router ?? Router() {
+    addScriptTag(true);
+  }
+
+  PheasantRouterApp router(Router router) {
+    _router = router;
+    return this;
+  }
+}
